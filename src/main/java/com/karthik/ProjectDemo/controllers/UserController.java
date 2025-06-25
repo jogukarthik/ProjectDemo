202f96671800224ca3243a337504bde51e473fc5
Jogu_Saikumar <Jogu.Saikumar@skillassure.com>
2025-06-25 23:09:43 +0530

    responseDto file modified

--- src/main/java/com/karthik/ProjectDemo/controllers/UserController.java
+++ src/main/java/com/karthik/ProjectDemo/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.karthik.ProjectDemo.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +40,6 @@ public class UserController {
         userdata=userService.postUser(requestDTO.getEmail(),requestDTO.getName(),requestDTO.getPassword());
         ResponseDTO user=new ResponseDTO();
         user.setEmail(userdata.getEmail());
-        user.setName(userdata.getName());
         return ResponseEntity.status(200).body(user);
     }
 }
